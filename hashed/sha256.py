40417a603214d72bb5146593c920e0bf2c5a2287
mukund26 <m.agarwalhp@gmail.com>
2024-04-09 21:31:53 +0530

    fixed file digest

--- hashed/sha256.py
+++ hashed/sha256.py
@@ -1,6 +1,6 @@
+import os
 from hashed.hash import Hash
-from hashed.common import string_to_binary, create_blocks, string_to_int, right_rotate, right_shift, binaries_to_binary_string
-from hashed.buffer_reader import BufferedReader
+from hashed.common import get_file_size, read_string_msg, right_rotate, right_shift
 
 class SHA256(Hash):
     
@@ -17,10 +17,11 @@ class SHA256(Hash):
             0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
             0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
         ]
-        self.MOD = 2**32
+        self.MOD = 0xFFFFFFFF
         self.BLOCK_SIZE = 512
         self.HASHED_BITS = 0
         self.DIGEST_SIZE= 32
+        
         
     def set_H(self): 
         self.H = [
@@ -28,142 +29,122 @@ class SHA256(Hash):
             0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
         ]
         
-    def create_message_schedule(self, block):
-        w = []
+    
+    def add_padding(self, binary_data, length = 0):
+        orig_len = len(binary_data) * 8  # Length in bits
+        binary_data.append(0x80)  # Append 1 bit  
+        no_of_zeros = ((self.BLOCK_SIZE - (len(binary_data) * 8 + 64) % self.BLOCK_SIZE) % self.BLOCK_SIZE) // 8
+        binary_data.extend(bytearray(no_of_zeros))
+        if length:
+            binary_data.extend(length.to_bytes(8, 'big'))
+        else:
+            binary_data.extend(orig_len.to_bytes(8, 'big'))
         
-        for i in range(0, 512, 32):
-            w.append(string_to_int(block[i : i + 32]))
-                    
+       
+    def create_message_schedule(self, block):
+        w = [int.from_bytes(block[i * 4 : i * 4 + 4], 'big') for i in range(0, 16)]
+            
         for i in range(16, 64):
             s0 = right_rotate(w[i - 15], 7) ^ right_rotate(w[i - 15], 18) ^ right_shift(w[i - 15], 3)
             s1 = right_rotate(w[i - 2], 17) ^ right_rotate(w[i - 2], 19) ^ right_shift(w[i - 2], 10)
-            ans = (w[i - 16] + s0 + w[i - 7] + s1) % self.MOD
+            ans = (w[i - 16] + s0 + w[i - 7] + s1) & self.MOD
             w.append(ans)
             
         return w
 
 
     def compression(self, w):
-        a = self.H[0]
-        b = self.H[1]
-        c = self.H[2]
-        d = self.H[3]
-        e = self.H[4]
-        f = self.H[5]
-        g = self.H[6]
-        h = self.H[7]    
+        a, b, c, d, e, f, g, h = self.H   
             
         for i in range(0, 64):
             s1 = right_rotate(e, 6) ^ right_rotate(e, 11) ^ right_rotate(e, 25)
             ch = (e & f) ^ ((~e) & g)
-            temp1 = (h + s1 + ch + self.K[i] + w[i]) % self.MOD
+            temp1 = (h + s1 + ch + self.K[i] + w[i]) & self.MOD
             s0 = right_rotate(a, 2) ^ right_rotate(a, 13) ^ right_rotate(a, 22)
             maj = (a & b) ^ (a & c) ^ (b & c)
-            temp2 = (s0 + maj) % self.MOD
+            temp2 = (s0 + maj) & self.MOD
             
             h = g
             g = f
             f = e
-            e = (d + temp1) % self.MOD
+            e = (d + temp1) & self.MOD
             d = c
             c = b
             b = a
-            a = (temp1 + temp2) % self.MOD
+            a = (temp1 + temp2) & self.MOD
             
-        self.H[0] = (self.H[0] + a) % self.MOD
-        self.H[1] = (self.H[1] + b) % self.MOD
-        self.H[2] = (self.H[2] + c) % self.MOD
-        self.H[3] = (self.H[3] + d) % self.MOD
-        self.H[4] = (self.H[4] + e) % self.MOD
-        self.H[5] = (self.H[5] + f) % self.MOD
-        self.H[6] = (self.H[6] + g) % self.MOD
-        self.H[7] = (self.H[7] + h) % self.MOD
+        self.H = [
+            (self.H[0] + a) & self.MOD,
+            (self.H[1] + b) & self.MOD,
+            (self.H[2] + c) & self.MOD,
+            (self.H[3] + d) & self.MOD,
+            (self.H[4] + e) & self.MOD,
+            (self.H[5] + f) & self.MOD,
+            (self.H[6] + g) & self.MOD,
+            (self.H[7] + h) & self.MOD
+        ]
     
     
-    def update(self, binary_string):
-        orig_len = len(binary_string)
+    def update(self, binary_data, length = 0, last_chunk = True):
+        if last_chunk:
+            self.add_padding(binary_data, length)
+
+        assert (len(binary_data) * 8) % self.BLOCK_SIZE == 0
         
-        binary_string += '1'
-    
-        L = len(binary_string)
+        blocks = [binary_data[ i : i + 64 ] for i in range(0, len(binary_data), 64)]
         
-        x = (self.BLOCK_SIZE - (L + 64) % self.BLOCK_SIZE) % self.BLOCK_SIZE
-    
-        binary_string += '0' * x
-            
-        #append 64 bit value of no of bits in original msg
-        binary_string += ''.join(bin(orig_len)[2:].zfill(64))
-                    
-        # create 512 bit blocks
-        blocks = create_blocks(binary_string, self.BLOCK_SIZE)
-                    
-        #sha computation
         for block in blocks:
             w = self.create_message_schedule(block)
             self.compression(w)
         
-        self.HASHED_BITS += len(binary_string)
+        self.HASHED_BITS = length if length else len(binary_data)
         
         
     def digest(self, msg):
-        if isinstance(msg, str):
-            binary_string = string_to_binary(msg)
-        elif not isinstance(msg, bytearray):
-            raise TypeError("Invalid message type") 
+        binary_data = read_string_msg(msg)
         
         self.HASHED_BITS = 0
         
-        self.update(binary_string)
+        self.update(binary_data)
         
         FINAL_H = self.H
         
         self.set_H()
         
-        return ((FINAL_H[0]).to_bytes(4, 'big') + (FINAL_H[1]).to_bytes(4, 'big') +
-                (FINAL_H[2]).to_bytes(4, 'big') + (FINAL_H[3]).to_bytes(4, 'big') +
-                (FINAL_H[4]).to_bytes(4, 'big') + (FINAL_H[5]).to_bytes(4, 'big') +
-                (FINAL_H[6]).to_bytes(4, 'big') + (FINAL_H[7]).to_bytes(4, 'big'))  
+        return b''.join(x.to_bytes(4, 'big') for x in FINAL_H)  
         
     
     def hex_digest(self, msg):
-        if isinstance(msg, str):
-            binary_string = string_to_binary(msg)
-        elif not isinstance(msg, bytearray):
-            raise TypeError("Invalid message type")
+        binary_data = read_string_msg(msg)
         
         self.HASHED_BITS = 0
         
-        self.update(binary_string)
+        self.update(binary_data)
         
         FINAL_H = self.H
         
         self.set_H()
-         
-        return ((FINAL_H[0]).to_bytes(4, 'big') + (FINAL_H[1]).to_bytes(4, 'big') +
-                (FINAL_H[2]).to_bytes(4, 'big') + (FINAL_H[3]).to_bytes(4, 'big') +
-                (FINAL_H[4]).to_bytes(4, 'big') + (FINAL_H[5]).to_bytes(4, 'big') +
-                (FINAL_H[6]).to_bytes(4, 'big') + (FINAL_H[7]).to_bytes(4, 'big')).hex()
+        
+        return b''.join(x.to_bytes(4, 'big') for x in FINAL_H).hex()
         
         
     def file_digest(self, filename, isBinary):
-        flag = 'r' if not isBinary else 'rb'
         self.HASHED_BITS = 0
-        with open(filename, flag) as f:
-            reader = BufferedReader(f)
+        length = get_file_size(filename)
+        pos = 0
+        with open(filename, 'rb' if isBinary else 'r') as f:
             while True:
-                data = reader.read(16384)
+                data = f.read(64)
+                pos += len(data)
                 if not data:
                     break
-                binary_string = string_to_binary(data) if not isBinary else binaries_to_binary_string(data)
-                self.update(binary_string)
-                
+                byte_data = bytearray(data) if isBinary else bytearray(data, 'ascii')
+                last_chunk = pos >= length
+                self.update(byte_data, length * 8, last_chunk)
+        # f = open(filename, 'rb' if isBinary else 'r')
+        # data = bytearray(f.read()) if isBinary else bytearray(f.read(), 'ascii')
+        # self.update(data)
         FINAL_H = self.H
-        
         self.set_H()
-        
-        return ((FINAL_H[0]).to_bytes(4, 'big') + (FINAL_H[1]).to_bytes(4, 'big') +
-                (FINAL_H[2]).to_bytes(4, 'big') + (FINAL_H[3]).to_bytes(4, 'big') +
-                (FINAL_H[4]).to_bytes(4, 'big') + (FINAL_H[5]).to_bytes(4, 'big') +
-                (FINAL_H[6]).to_bytes(4, 'big') + (FINAL_H[7]).to_bytes(4, 'big')).hex()       
+        return b''.join(x.to_bytes(4, 'big') for x in FINAL_H).hex()    
             
-    
