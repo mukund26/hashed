fcebc50d20cf5d245f9ff7edd026252331101fe2
mukund26 <m.agarwalhp@gmail.com>
2024-04-09 21:52:17 +0530

    updated common

--- hashed/common.py
+++ hashed/common.py
@@ -1,24 +1,18 @@
+import os
+
 def right_rotate(num, val, size = 32):
     return num >> val | num << (size - val)
 
 def right_shift(num, val):
     return num >> val
 
-def string_to_binary(msg):
-    binary = []
-    for ch in msg:
-        binary.append(bin(ord(ch))[2:].zfill(8))
-    return ''.join(binary)
+def get_file_size(file_path):
+        size = os.path.getsize(file_path)
+        return size
 
-def binaries_to_binary_string(binary_data):
-    binary_string = ''.join(format(byte, '08b') for byte in binary_data)
-    return binary_string
-
-def create_blocks(binary_string, block_size = 512):
-    blocks = []
-    for i in range(0, len(binary_string), block_size):
-        blocks.append(binary_string[i : i + block_size])
-    return blocks
-
-def string_to_int(binary_str):
-    return int(binary_str, 2)
+def read_string_msg(msg):
+    if isinstance(msg, str):
+        return bytearray(msg, 'ascii')
+    elif not isinstance(msg, bytearray):
+        raise TypeError("Invalid message type")
+     
